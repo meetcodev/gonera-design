8cbaa4c96de838d6a38abb52a852be5ead7fc68f
Marek Mitko <mitko.email@gmail.com>
2023-04-16 18:04:40 +0200

    feat(StainHamburger): Hide active path

--- components/sidebar/StainHamburger.jsx
+++ components/sidebar/StainHamburger.jsx
@@ -13,8 +13,17 @@ import { isActiveLink } from "../../utilis/linkActiveChecker";
 
 const StainHamburger = () => {
     const router = useRouter();
+
+    
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
+
+    // const  timer = () => {
+    //     setTimeout(() => {
+    //         handleClick();
+    //     },1000);
+    // };
+
 
     return(
         <>
@@ -22,27 +31,32 @@ const StainHamburger = () => {
             <label for="menu-icon"></label>
             <nav class="nav" id="stain-hamburger--nav"> 	
                 <ul className="pt-5 ulMenuList">
-                   {sidebarData.map((item) => (
-                    <li key={item.id}  className="liMenuItem" onClick={handleClick}>
-                    <Link
-                        className={`${
-                            isActiveLink(item.routePath, router.asPath)
-                            ? "active "
-                            : ""
-                        }`}
-                        href={item.routePath}
-                    >
-                        {/* <Image
-                        width={15}
-                        height={15}
-                        className="svg"
-                        src={item.icon}
-                        alt="homerun"
-                        /> */}
-                            <span className="menu_content">{item.menuName}</span>
-                    </Link>
-                    </li>
-                ))}
+                    {sidebarData.map((item) => (
+                        <li key={item.id}  className="liMenuItem" onClick={handleClick}  
+                            style={{ display: ((item.routePath === router.asPath) || ( item.routePath === "/home" ) && (router.asPath === "/")) ? 'none' : "" }}
+                        >
+                        <Link   
+                            className={`${
+                                isActiveLink(item.routePath, router.asPath)
+                                ? "active "
+                                : ""
+                            }`}
+                            // style={{isActiveLink(item.routePath, router.asPath) ? }}
+                            // {router.asPath !== "/portfolio" ? <ThemeSwitch /> : ""}
+                            href={item.routePath}
+                        >
+                            {/* <Image
+                            width={15}
+                            height={15}
+                            className="svg"
+                            src={item.icon}
+                            alt="homerun"
+                            /> */}
+                                <span className="menu_content">{item.menuName}</span>
+                        </Link>
+                        </li>
+                    ) 
+                )}
                 </ul>  
             </nav>
         </>
